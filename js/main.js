d74a5cc31032b96abb5a064adf440302317984ce
RWDD-Sajal-Acharjee <itsajal2017@gmail.com>
2021-11-25 03:06:36 +0600

    Gymnasium Project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
     'usr strict'
-    
+
+
+    // banner slider
     $('.banner_slider').slick({
         autoplay: true,
         arrows: false,
         dots: true,
+    });
+
+    // venobox
+    $('.venobox').venobox({
+        border     : '2px',
+        bgcolor    : '#E23E38',
+        titleattr  : 'data-title',
+        titleColor : 'white',
+        closeColor : 'red',
+        spinner    : 'wandering-cubes',
+        spinColor  : '#E23E38',
+        share      : ['facebook', 'twitter', 'download']
     });
 })
