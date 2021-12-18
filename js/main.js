962675948dbd03b9de395d386564f94bd37503ce
RWDD-Sajal-Acharjee <itsajal2017@gmail.com>
2021-12-19 01:02:05 +0600

    Gymnassium Project

--- js/main.js
+++ js/main.js
@@ -1,12 +1,54 @@
 $(document).ready(function(){
     'usr strict'
 
+    // color switcher
+    $('.color-picker .color-btn').on('click',function(){
+        $('.color-picker').toggleClass('color-class');
+    })
+
+    $('.color-picker ul li.orange').on('click', function(){
+        $('body').addClass('orange').removeClass('blue').removeClass('green');
+    })
+
+    $('.color-picker ul li.blue').on('click', function(){
+        $('body').addClass('blue').removeClass('orange').removeClass('green');
+    })
+
+    $('.color-picker ul li.green').on('click', function(){
+        $('body').addClass('green').removeClass('orange').removeClass('blue');
+    })
+
+    $('.color-picker ul li.default').on('click', function(){
+        $('body').removeClass('orange').removeClass('blue').removeClass('green');
+    })
+
+    // scroll function
+    $(window).on('scroll',function(){
+        var scrolling = $(this).scrollTop();
+        if(scrolling >=150){
+            $('#NAVBAR').fadeOut();
+            $('.menu-bar').fadeIn();
+        }else{
+            $('#NAVBAR').fadeIn();
+            $('.menu-bar').fadeOut();
+        }
+    })
+
 
     // banner slider
     $('.banner_slider').slick({
         // autoplay: true,
         arrows: false,
         dots: true,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    autoplay: false,
+                }
+            },
+        ]
     });
 
     // venobox
@@ -61,18 +103,49 @@ $(document).ready(function(){
         arrows: true,
         dots: false,
         slidesToShow: 5,
+        slidesToScroll: 3,
+        autoplay: true,
         centerMode: true,
         centerPadding: '0',
         prevArrow: '<i class="fas fa-chevron-left slick-prev left-arrow"></i>',
         nextArrow: '<i class="fas fa-chevron-right slick-prev right-arrow"></i>',
+
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 768,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    arrows: false,
+                    autoplay: true,
+                    centerMode: true,
                 }
             },
         ],
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    arrows: false,
+                    autoplay: true,
+                    centerMode: false,
+                }
+            },
+        ],
+
+        
+
+        // responsive: [
+        //     {
+        //         breakpoint: 576,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             arrows: false,
+        //             autoplay: true,
+        //             centerMode: false,
+        //         }
+        //     },
+        // ],
     })
 })
